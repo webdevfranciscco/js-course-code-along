880413f26716b8481340132df1a1d7c128612238
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-20 13:58:10 -0600

    data types summary

---
+++ 01-Fundamentals-Part-1/data-types.js
@@ -0,0 +1,28 @@
+/*
+    Data Types in JavaScript
+    
+    1. Number: Floating point numbers, used for decimal numbers, but also for integers, because JavaScript does not have a true integer data type.
+    
+    Example: let age = 23;
+
+    2. String: A sequence of characters. Used for text. 
+    
+    Example: let firstName = 'Franciscco';
+    
+    3. Boolean: Logical type that can only be true or false. Used for making decisions.
+    
+    Example: let isGreen = true;";
+    
+    4. Undefined: Value taken by a variable that is not yet defined ('empty value').
+
+    5. Null: Also means 'empty value', but more on this later.
+
+    6. Symbol (ES2015): Value that is unique and cannot be changed, not useful for now. More on it later.
+
+    7. BigInt (ES2020): Larger integers than the Number type can hold.
+
+    In JS, we do not have to manually define the  data type of the values stored in a variable. Instead, data types are determined automatically, because JS has what is called dynamic typing.
+    
+    Even more, in JS is the value who has the type, NOT the variable! This can be the source of many bugs in code.
+*/
+
