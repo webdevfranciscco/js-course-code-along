dc327b24615c14fe3b5cba99ac2d4ed1059c57bc
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-20 14:49:47 -0600

    data types and typeof in action

--- 01-Fundamentals-Part-1/data-types.js
+++ 01-Fundamentals-Part-1/data-types.js
@@ -24,5 +24,24 @@
     In JS, we do not have to manually define the  data type of the values stored in a variable. Instead, data types are determined automatically, because JS has what is called dynamic typing.
     
     Even more, in JS is the value who has the type, NOT the variable! This can be the source of many bugs in code.
+
+    The keyword typof can be used to obtain the type of a value or variable.
 */
 
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Francisco');
+
+let codingIsFun = true;
+
+console.log(codingIsFun);
+console.log(typeof codingIsFun);
+
+codingIsFun = 'YES!';
+
+console.log(codingIsFun);
+console.log(typeof codingIsFun);
