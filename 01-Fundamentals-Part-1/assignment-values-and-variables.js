eec1b2f5ea1c063d905ad212f383ad3f7c2b8ed5
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-20 12:50:29 -0600

    assignment on values and variables

---
+++ 01-Fundamentals-Part-1/assignment-values-and-variables.js
@@ -0,0 +1,17 @@
+/*
+    Values and Variables
+
+    1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+    2. Log their values to the console.
+*/
+
+let country = "Mexico";
+let continent = "America";
+let population = "130"; //population is in millions
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log('My country is ' + country + ", it is located in the continent of " + continent + " and it has a population of " + population + " million.");
