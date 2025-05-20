8c66dc03a2e08ab669ae7ab8fa4d51f4338e457e
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-20 10:10:42 -0600

    'what is a variable' demo

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,11 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Javascript");
+console.log(37);
+
+let firstName = "React";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
