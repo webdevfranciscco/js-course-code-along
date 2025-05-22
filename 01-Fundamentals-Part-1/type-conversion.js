71c78d7a33cc378ec010f9fbe4fbba646652daf7
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-22 17:39:51 -0600

    conversion to string and its display versus a number in the console

--- 01-Fundamentals-Part-1/type-conversion.js
+++ 01-Fundamentals-Part-1/type-conversion.js
@@ -8,4 +8,9 @@ console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
 
 console.log(Number('Jonas'));
+
 console.log(typeof NaN);
+
+console.log('This is a string:', String(23));
+
+console.log('This is a number:', 23);
