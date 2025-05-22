2ac3f1abd62f959b6bd7f0e08316fd0d25c71cde
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-22 16:35:12 -0600

    example of block scoped variable definition

--- 01-Fundamentals-Part-1/if-else.js
+++ 01-Fundamentals-Part-1/if-else.js
@@ -7,3 +7,12 @@ if (age >= 18) {
     const yearsLeft = 18 - age;
     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
 }
+
+const birthYear = 1991;
+if (birthYear <= 2000) {
+    let century = 20;
+} else {
+    let century = 21;
+}
+
+console.log(century);
