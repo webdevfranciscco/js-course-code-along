33c5768d16e1b442118d35b713aaec05e70d8820
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-22 17:50:39 -0600

    type coercion and arithmetic operators

--- 01-Fundamentals-Part-1/type-conversion.js
+++ 01-Fundamentals-Part-1/type-conversion.js
@@ -1,4 +1,6 @@
 
+// type conversion
+
 const inputYear = '1991';
 
 console.log(inputYear + 18);
@@ -14,3 +16,11 @@ console.log(typeof NaN);
 console.log('This is a string:', String(23));
 
 console.log('This is a number:', 23);
+
+// type coersion
+
+console.log('I am ' + 23 + ' years old.');
+console.log('23' - '10' - 3); // output: 10 (as a number)
+console.log('23' + '10' + 3); // output: 23103 (as a string)
+console.log('23' * '10'); // output: 230 (as a number)
+console.log('23' / '10'); // output: 2.3 (as a number)
