dd602964bac20916fc99b95c4972723a5c5f19c6
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-22 17:27:35 -0600

    type conversion and the odd behavior of NaN

---
+++ 01-Fundamentals-Part-1/type-conversion.js
@@ -0,0 +1,11 @@
+
+const inputYear = '1991';
+
+console.log(inputYear + 18);
+
+console.log(Number(inputYear), inputYear);
+
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
