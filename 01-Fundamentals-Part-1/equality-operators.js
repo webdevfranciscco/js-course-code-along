555034d9fcdde0c6b478458059202a7f3a2c3292
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-23 12:02:08 -0600

    equality variants - another example of an equality fail

--- 01-Fundamentals-Part-1/equality-operators.js
+++ 01-Fundamentals-Part-1/equality-operators.js
@@ -1,30 +1,63 @@
+/*
+    let age = '18'; // Using this as a string...
 
-let age = '18'; // Using this as a string...
+    console.log(`age = ${age}`);
 
-console.log(`age = ${age}`);
+    if (age === 18) { // ... causes this to be false...
+        console.log('You just became an adult :D (strict)');
+    } else console.log('You are too young (strict)');
 
-if (age === 18) { // ... causes this to be false...
-    console.log('You just became an adult :D (strict)');
-} else console.log('You are too young (strict)');
+    if (age == 18) { // ... but this to be true...
+        console.log('You just became an adult :D (loose)');
+    } else console.log('You are too young (loose)');
 
-if (age == 18) { // ... but this to be true...
-    console.log('You just became an adult :D (loose)');
-} else console.log('You are too young (loose)');
+    age = '17';
 
-age = '17';
+    console.log(`age = ${age}`);
 
-console.log(`age = ${age}`);
+    if (age === 18) { // ... this is also false (a string is not equal to a number)...
+        console.log('You just became an adult :D (strict)');
+    } else console.log('You are too young (strict)');
 
-if (age === 18) { // ... this is also false (a string is not equal to a number)...
-    console.log('You just became an adult :D (strict)');
-} else console.log('You are too young (strict)');
+    if (age == 18) { // ... as this is (coerced number 17 is not equal to 18)...
+        console.log('You just became an adult :D (loose)');
+    } else console.log('You are too young (loose)');
 
-if (age == 18) { // ... as this is (coerced number 17 is not equal to 18)...
-    console.log('You just became an adult :D (loose)');
-} else console.log('You are too young (loose)');
+    // ... only they are false for different reasons, yet still they produce the same result.
 
-// ... only they are false for different reasons, yet still they produce the same result.
+    // this can be an easy source of bugs and confusion when trying to track them down.
 
-// this can be an easy source of bugs and confusion when trying to track them down.
+    // It is better to ALWAYS USE === to test for equality. If type conversion is a choice, make it explicit by using a conversion, not by allowing coercion.
+*/
 
-// It is better to ALWAYS USE === to test for equality. If type conversion is a choice, make it explicit by using a conversion, not by allowing coercion.
+let favorite = prompt("What's your favorite number?");
+
+console.log(typeof favorite);
+console.log(`The variable named favorite was set to ${favorite} `);
+
+if (favorite == 23) {
+    console.log('You chose number 23.')
+
+} else console.log("You didn't chose  number 23.")
+
+if (favorite === 23) {
+    console.log('You chose number 23.')
+
+} else console.log("You didn't chose  number 23.")
+
+favorite = prompt("What's your favorite number?");
+
+console.log(typeof favorite);
+console.log(`The variable named favorite was set to ${favorite} `);
+
+if (favorite == 23) {
+    console.log('You chose number 23.')
+
+} else console.log("You didn't chose  number 23.")
+
+if (favorite === 23) {
+    console.log('You chose number 23.')
+
+} else console.log("You didn't chose  number 23.")
+
+// There are 2 prompts in this example. Enter the number 3 on the first prompt window and 23 in the second...
