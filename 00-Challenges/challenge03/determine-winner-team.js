00c41c2cec933bf9848a4a054ffaf42a723e3f2d
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-23 18:26:28 -0600

    challenge-03 - add comment with challenge description

--- 00-Challenges/challenge03/determine-winner-team.js
+++ 00-Challenges/challenge03/determine-winner-team.js
@@ -1,3 +1,20 @@
+/*
+    Coding Challenge #3
+
+    There are two gymnastics teams, Dolphins and Koalas. They compete against each
+    other 3 times. The winner with the highest average score wins a trophy!
+
+    Your tasks:
+
+    1. Calculate the average score for each team, using the test data below
+    2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that
+    as well (draw means they have the same average score)
+
+    Test data:
+    
+    Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+*/
+
 let dolphinsAverageScore = (96 + 108 + 89) / 3;
 let koalasAverageScore = (88 + 91 + 110) / 3;
 
