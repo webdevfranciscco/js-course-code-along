fc8706f035017071d7897bd2798bb0b080a36c62
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-26 17:54:12 -0600

    conditional or ternary operator - example demonstrating the benefits of the ternary operator

--- 01-Fundamentals-Part-1/conditional-or-ternary-operator.js
+++ 01-Fundamentals-Part-1/conditional-or-ternary-operator.js
@@ -1,5 +1,5 @@
 
-const age = 15;
+const age = 25;
 
 /*
     age >= 18 ? console.log('I like to drink wine.') : console.log('I like to drink water.')
@@ -9,3 +9,13 @@ const age = 15;
 
 const drink = age >= 18 ? 'wine' : 'water';
 console.log(drink);
+
+// compare this last code to the following to understand the benefit of the ternary operator
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
