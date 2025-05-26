99ff4442de87e2a9b2dd1fcdeef9f3d8ab0aa158
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-26 17:48:46 -0600

    conditional or ternary operator - use to conditionally declare a variable

--- 01-Fundamentals-Part-1/conditional-or-ternary-operator.js
+++ 01-Fundamentals-Part-1/conditional-or-ternary-operator.js
@@ -1,4 +1,11 @@
 
 const age = 15;
 
-age >= 18 ? console.log('I like to drink wine.') : console.log('I like to drink water.')
+/*
+    age >= 18 ? console.log('I like to drink wine.') : console.log('I like to drink water.')
+*/
+
+// conditional declaration of a variable
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
