fe2da73e3d29534329e7ef1a4603185b36511c17
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-26 17:36:51 -0600

    expression versus statement

---
+++ 01-Fundamentals-Part-1/expression-vs-statement.js
@@ -0,0 +1,10 @@
+
+/*
+
+  Understanding the difference between statements and expressions in JavaScript is critical for writing efficient and clean code.
+  
+  While expressions return values, statements perform actions. 
+  
+  Knowing when to use each, and how they can interact, will help you better structure your JavaScript programs.
+
+*/
