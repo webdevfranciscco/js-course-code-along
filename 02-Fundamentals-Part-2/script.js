217e6c639b53d147e996a84c7f0f4b868fa45628
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-27 10:08:44 -0600

    enable 'strict mode'

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let hasdriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; // intentional omission of an s letter to test strict mode
+if (hasSriversLicense) console.log('I can drive :D');
