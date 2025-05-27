f7608c8a37f4bf6dfbfa68a75c4dacce003c24cd
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-27 11:45:08 -0600

    arrow functions - an example with a block of code within brackets

--- 02-Fundamentals-Part-2/functions.js
+++ 02-Fundamentals-Part-2/functions.js
@@ -1,28 +1,12 @@
 'use strict';
 
 
-// function declaration
-
-
-const age1 = calcAge1(1991);
-console.log(`The age from the function declaration is ${age1} years.`);
-
-function calcAge1(birthYear) {   // this is a declaration, it can be placed anywhere in the code and the interpreter will process it first before executing any code, so it can be called in the code even before it is listed, like we are doing here
-    return 2037 - birthYear;
-}
-
-
-// function expression
-const calcAge2 = function (birthYear) {
-    return 2037 - birthYear;  // this function is an expression because it returns a value... functions are not actually a type of object, but values
-}
-
-const age2 = calcAge2(1998);
-console.log(`The age from the function expression is ${age2} years.`);
-
-
 // arrow function
-const calcAge3 = birthYear => 2037 - birthYear;
 
-const age3 = calcAge3(1971);
-console.log(`The age from the function expression is ${age3} years.`);
+const yearsUntilRetirement = birthYear => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    return retirement;
+}
+
+console.log(`Years to retirement: ${yearsUntilRetirement(1991)}`);
