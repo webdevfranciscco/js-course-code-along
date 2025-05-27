237a83a5d84d26ae2b08edb928f33e4aaa507bb9
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-27 10:15:28 -0600

    write our very first function

---
+++ 02-Fundamentals-Part-2/functions.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// call / run / invoke are three ways to refer to executing a function
+logger();
+logger();
+logger();
