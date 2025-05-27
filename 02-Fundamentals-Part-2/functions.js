2776a83e9c7077cf93ff63b5476cf65617249413
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-27 11:38:15 -0600

    arrow functions - one liner

--- 02-Fundamentals-Part-2/functions.js
+++ 02-Fundamentals-Part-2/functions.js
@@ -11,6 +11,7 @@ function calcAge1(birthYear) {   // this is a declaration, it can be placed anyw
     return 2037 - birthYear;
 }
 
+
 // function expression
 const calcAge2 = function (birthYear) {
     return 2037 - birthYear;  // this function is an expression because it returns a value... functions are not actually a type of object, but values
@@ -18,3 +19,10 @@ const calcAge2 = function (birthYear) {
 
 const age2 = calcAge2(1998);
 console.log(`The age from the function expression is ${age2} years.`);
+
+
+// arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+
+const age3 = calcAge3(1971);
+console.log(`The age from the function expression is ${age3} years.`);
