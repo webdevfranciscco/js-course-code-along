c5463f91fd8edb0acb6026bafca7c6da23bcac1c
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-28 12:01:50 -0600

    arrays - pop method

--- 02-Fundamentals-Part-2/arrays-basic-operations.js
+++ 02-Fundamentals-Part-2/arrays-basic-operations.js
@@ -3,14 +3,33 @@ const friends = ['Michael', 'Steven', 'Peter'];
 console.log(friends);
 console.log(`Length = ${friends.length}`);
 
+// ** Add elements *******************************
 // push method adds element at the end of an array
 const newLengthPush = friends.push('Jay');
 
 console.log(friends);
-console.log(`New length = ${newLengthPush}`);
+console.log(`push - New length = ${newLengthPush}`);
 
 // unshift method adds element at the beginning of an array
 const newLengthUnshift = friends.unshift('John');
 
+
 console.log(friends);
-console.log(`New length = ${newLengthUnshift}`);
+console.log(`unshift - New length = ${newLengthUnshift}`);
+
+// ** Remove elements ****************************
+
+// pop removes the last element of an array and returns its value
+const poppedElement = friends.pop();
+const newLengthPop = friends.length;
+
+console.log(friends);
+console.log(`pop - New length = ${newLengthPop}`);
+console.log(`Popped element = ${poppedElement}`);
+
+
+
+
+
+
+
