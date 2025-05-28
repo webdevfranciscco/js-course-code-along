f7cb50287ea787029082cc0115fa6d162934a8b9
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-28 17:45:38 -0600

    challenge 02 of section 02 - tip calculator using a function

---
+++ 02-challenges/challenge02/tip-calculator-01.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/*
+    CHALLENGE #2
+
+    Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+    Your tasks:
+
+    1. Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+    2. And now let's use arrays! So, create an array called bills containing the test data below.
+
+    3. Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+    4. BONUS: Create an array totals containing the total values, so the bill + tip.
+
+    TEST DATA: 125, 555, and 44.
+*/
+
+
+console.log('tip calculator');
+
+let bill = 100;
+
+const calcTip = function (bill) {
+    return (bill >= 50 && bill <= 300) ? bill * 15 / 100 : bill * 20 / 100;
+}
+
+console.log(`For a bill of $${bill}, the tip is $${calcTip(bill)} and the total is $${bill + calcTip(bill)}.`);
+
+
+
