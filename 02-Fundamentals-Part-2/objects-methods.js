8ae157cc0ae07b0f7e7b3cfc1bc06f6632aea04f
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-29 11:46:40 -0600

    objects - methods - little challenge - form a sentence out of an object's data with a new method within the object

--- 02-Fundamentals-Part-2/objects-methods.js
+++ 02-Fundamentals-Part-2/objects-methods.js
@@ -10,9 +10,23 @@ const jonasObject = {
     calcAge: function (birthYear) {
         this.age = 2037 - this.birthYear;
         return this.age;
+    },
+
+    logNameAndLicenseStatus: function () {
+        if (this.hasDriversLicense) {
+            licenseStatus = 'a';
+        } else licenseStatus = 'no';
+
+        return `${this.firstName} is a ${this.calcAge()} year old ${this.job}, and he has ${licenseStatus} driver's license.`;
     }
 };
 
-console.log(jonasObject.calcAge());
+// Challenge
 
-console.log(jonasObject.age);
+// Produce the statement:
+
+// "Jonas is a 46 year old teacher, and he has a/no driver's license"
+
+// with a new method on the jonasObject using the object's data
+
+console.log(jonasObject.logNameAndLicenseStatus());
