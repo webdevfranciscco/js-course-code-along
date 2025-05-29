a680a7df3f2d8d2fc03c7c8e01e21fefe812a2b2
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-28 18:24:26 -0600

    objects - properties

---
+++ 02-Fundamentals-Part-2/objects.js
@@ -0,0 +1,20 @@
+
+// Arrays have elements and are indexed by a number
+
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+// Objects contain properties, each formed by a key/value pair (i.e. firstName/'Jonas')
+
+const jonasObject = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
