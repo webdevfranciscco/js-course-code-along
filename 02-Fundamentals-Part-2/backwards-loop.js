985ab16be2695933c9ab91d5568b6637226d34cd
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-30 09:50:38 -0600

    the for loop - backwards loops

---
+++ 02-Fundamentals-Part-2/backwards-loop.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const jonas = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+]
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
