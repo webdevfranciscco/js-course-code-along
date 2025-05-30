5b6f1f66058ee680cab93e700c6b2e37f6d8b284
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-30 10:28:39 -0600

    the while loop - use case without a counter - dice rolling until we get a 6

--- 02-Fundamentals-Part-2/while-loop.js
+++ 02-Fundamentals-Part-2/while-loop.js
@@ -5,3 +5,11 @@ while (rep <= 10) {
     console.log(`Lifting weight for repetition ${rep}`);
     rep++;
 }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+}
+console.log(`You rolled a ${dice}`);
