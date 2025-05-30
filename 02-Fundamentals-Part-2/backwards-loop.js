ad76f94f308209fd297fa8044ecd30d6cff16aa7
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-05-30 10:10:13 -0600

    the for loop - nested loops

--- 02-Fundamentals-Part-2/backwards-loop.js
+++ 02-Fundamentals-Part-2/backwards-loop.js
@@ -12,3 +12,19 @@ const jonas = [
 for (let i = jonas.length - 1; i >= 0; i--) {
     console.log(i, jonas[i]);
 }
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+    console.log(i, jonas[i]);
+}
+
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`--------- Exercise $(exercise) ---------`);
+
+
+
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight for repetition ${rep}`);
+    }
+}
