d3e756d42c77c1ec7d80d0173ffbeff00f2b1411
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-03 17:29:39 -0600

    how to fix a bug, modified C to K converter showing how to fix the bug by example

--- 05-developer-skills-n-editor-setup/celsius-to-kelvin.js
+++ 05-developer-skills-n-editor-setup/celsius-to-kelvin.js
@@ -4,11 +4,20 @@ const measurementKelvin = function () {
   const measurementCelsius = {
     type: 'temperature',
     unit: 'celsius',
-    value: prompt('Enter temperature in Celsius degrees:'),
+    // 3. This is where this has been fixed
+    // value: prompt('Enter temperature in Celsius degrees:')
+    // was changed to:
+    value: Number(prompt('Enter temperature in Celsius degrees:')),
   };
+
+  // 2. Find: this logs the measurementCelsius object, where it can be seen that
+  // measurementCelsius.value is a string, not a number as expected
+  // the error results from a type coercion of 273 into a string, followed by a concatenation (hence the 10273 result)
+  console.log(measurementCelsius);
 
   const kelvin = measurementCelsius.value + 273;
   return kelvin;
 };
 
+// 1. Identify: this comand logs 10273 instead of the expected 283.
 console.log(measurementKelvin());
