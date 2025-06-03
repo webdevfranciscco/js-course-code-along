7dd140d85b2d2595ea8e9f97d46817ebfb83b207
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-03 11:55:19 -0600

    add problem definition to the temperature amplitud script file

--- 05-developer-skills-n-editor-setup/temperature-amplitude.js
+++ 05-developer-skills-n-editor-setup/temperature-amplitude.js
@@ -1,5 +1,22 @@
 'use strict';
 
+/* 
+    PROBLEM:
+
+    We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of the day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+    1) Understanding the problem
+        - What is temperature amplitude?
+            Answer: difference between highest and lowest temperatures
+        - How to compute max and min temperatures?
+        - What is a sensor error? And what to do?
+    2) Breaking up into sub-problems
+        - How to ignore errors?
+        - Find max value in temperatures array
+        - Find min value in temperatures array
+        - Substract min from max (amplitude) and return it.
+
+*/
 const calcTempAmplitude = function (temps) {
   let max = temps[0];
   let min = temps[0];
