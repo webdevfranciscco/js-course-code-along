58b2745f01088f82aa407634289924e88cacd8dc
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-09 12:41:12 -0600

    more DOM changes using querySelector, as well as the textContent and value properties

--- 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
@@ -4,4 +4,10 @@ console.log(document.querySelector(".message").textContent);
 
 document.querySelector(".message").textContent = "Correct number!!!"; // change the text content of the element with the 'message' class to 'Correct number!!!'
 
-console.log(document.querySelector(".message").textContent);
+document.querySelector(".number").textContent = 13;
+
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+
+console.log(document.querySelector(".guess").value);
