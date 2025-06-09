281f9105d1d554386bc67ad13e7af950632060b2
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-09 11:09:39 -0600

    change DOM element with class .message and add screenshots showing the initial state

--- 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
@@ -1,3 +1,7 @@
 "use strict";
 
-console.log(document.querySelector(".message"));
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct number!!!"; // change the text content of the element with the 'message' class to 'Correct number!!!'
+
+console.log(document.querySelector(".message").textContent);
