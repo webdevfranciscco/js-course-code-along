b3a64b61f7b8aa3f5521d9d3dac14455d5d3db11
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-10 09:39:45 -0600

    make the Check! button work by logging data from the input with class ".guess"

--- 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
@@ -1,13 +1,5 @@
 "use strict";
 
-console.log(document.querySelector(".message").textContent);
-
-document.querySelector(".message").textContent = "Correct number!!!"; // change the text content of the element with the 'message' class to 'Correct number!!!'
-
-document.querySelector(".number").textContent = 13;
-
-document.querySelector(".score").textContent = 10;
-
-document.querySelector(".guess").value = 23;
-
-console.log(document.querySelector(".guess").value);
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+});
