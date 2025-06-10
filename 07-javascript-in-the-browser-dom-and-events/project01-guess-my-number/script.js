f525a493bad4e7c5befee39f6f9e5071924cfd60
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-10 11:26:27 -0600

    add random generated number and temporarily log it to the ".number" <div>

--- 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
