d4dff83a3702b888df752f387ee9b6c69dcd2a8e
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-10 12:46:42 -0600

    add functionality to check if a guess is too high or too low

--- 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
@@ -1,7 +1,9 @@
 "use strict";
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -10,5 +12,29 @@ document.querySelector(".check").addEventListener("click", function () {
   if (!guess) {
     document.querySelector(".message").textContent =
       "Please,enter a number greater than zero!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "You guessed it!!!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent =
+        "Your number is too high ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      score--;
+      document.querySelector(".score").textContent = score;
+      document.querySelector(".message").textContent = "Game over! ☹️";
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent =
+        "Your number is too low ";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      score--;
+      document.querySelector(".score").textContent = score;
+      document.querySelector(".message").textContent = "Game over! ☹️";
+    }
   }
 });
