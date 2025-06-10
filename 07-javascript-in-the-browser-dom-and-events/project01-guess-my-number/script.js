b847680089d48e02a7855e004e8b7bf307b330e3
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-10 11:04:18 -0600

    change ".message" legend when a falsy is entered

--- 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
@@ -3,4 +3,9 @@
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent =
+      "Please,enter a number greater than zero!";
+  }
 });
