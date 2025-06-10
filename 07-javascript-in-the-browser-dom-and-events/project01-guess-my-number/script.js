f1fd8376c9e0410fe142bedaa977a87ec4b89a61
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-10 10:39:25 -0600

    refactor ".guess" button functionality

--- 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
 document.querySelector(".check").addEventListener("click", function () {
-  console.log(document.querySelector(".guess").value);
+  const guess = document.querySelector(".guess").value;
+  console.log(guess);
 });
