4355ae7604b76cc62f3cbb114bc95568864caa80
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-11 12:21:32 -0600

    challenge 01 of section 05 - fix hard-coded number with the score variable

--- 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
@@ -62,7 +62,7 @@ document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector(".message").textContent = "Start guessing...";
-  document.querySelector(".score").textContent = 20;
+  document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".number").style.width = "15rem";
   document.querySelector("body").style.backgroundColor = "#222";
