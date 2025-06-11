d6d34e57512840671dbcfa3a033110e86ba3fcf2
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-11 13:26:08 -0600

    challenge 01 of section 05 - my solution for the highscore value

--- 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project01-guess-my-number/script.js
@@ -18,6 +18,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -33,6 +34,10 @@ document.querySelector(".check").addEventListener("click", function () {
 
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highScore) {
+      document.querySelector(".highscore").textContent = score;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent =
