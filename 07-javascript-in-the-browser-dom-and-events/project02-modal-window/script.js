5a483e99a6138a4f897031633f42a56e32f86691
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-12 12:02:03 -0600

    add a listener to querySelectorAll (buttonsOpenModal) and log the clicked button

--- 07-javascript-in-the-browser-dom-and-events/project02-modal-window/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project02-modal-window/script.js
@@ -7,5 +7,8 @@ const buttonsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(buttonsOpenModal);
 
-for (let i = 0; i < buttonsOpenModal.length; i++)
-  console.log(i, buttonsOpenModal[i].textContent);
+for (let i = 0; i < buttonsOpenModal.length; i++) {
+  buttonsOpenModal[i].addEventListener('click', function () {
+    console.log(`Button ${i} clicked`);
+  });
+}
