22320e4fc92ed44572d973368fe8658b85b5cf1f
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-12 12:35:33 -0600

    add a listener to the overlay so it closes itself and the modal when clicked

--- 07-javascript-in-the-browser-dom-and-events/project02-modal-window/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project02-modal-window/script.js
@@ -19,3 +19,8 @@ buttonCloseModal.addEventListener('click', function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 });
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
