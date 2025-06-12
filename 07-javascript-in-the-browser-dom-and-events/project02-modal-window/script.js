30e7be0669cd21e8234bbc85eaffec9ee0a79d62
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-12 11:45:34 -0600

    setup variables for the query selector calls and traverse a querySelectorAll

--- 07-javascript-in-the-browser-dom-and-events/project02-modal-window/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project02-modal-window/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonCloseModal = document.querySelector('.close-modal');
+const buttonsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(buttonsOpenModal);
+
+for (let i = 0; i < buttonsOpenModal.length; i++)
+  console.log(i, buttonsOpenModal[i].textContent);
