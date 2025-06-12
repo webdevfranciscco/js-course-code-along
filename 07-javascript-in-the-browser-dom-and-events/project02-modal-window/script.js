8de71f51f2e3f17a0845a920f8e53d1b7806d11e
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-12 13:06:21 -0600

    refactor to make the open modal and overly code easier to read, and some cleanup

--- 07-javascript-in-the-browser-dom-and-events/project02-modal-window/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project02-modal-window/script.js
@@ -5,20 +5,19 @@ const overlay = document.querySelector('.overlay');
 const buttonCloseModal = document.querySelector('.close-modal');
 const buttonsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(buttonsOpenModal);
-
-for (let i = 0; i < buttonsOpenModal.length; i++) {
-  buttonsOpenModal[i].addEventListener('click', function () {
-    console.log(`Button ${i} clicked`);
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
 const closeModalAndOverlay = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+const openModalAndOverlay = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < buttonsOpenModal.length; i++) {
+  buttonsOpenModal[i].addEventListener('click', openModalAndOverlay);
+}
 
 buttonCloseModal.addEventListener('click', closeModalAndOverlay);
 
