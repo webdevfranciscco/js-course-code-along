aadfb3e275bee6b5635450a8fbad792ae7b9adcc
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-13 10:38:32 -0600

    select elements to set initial conditions on the game's board

--- 07-javascript-in-the-browser-dom-and-events/project03-pig-game/script.js
+++ 07-javascript-in-the-browser-dom-and-events/project03-pig-game/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+
+// select elements
+let domElementScore0 = document.querySelector('score--0');
+let domElementScore1 = document.getElementById('score--1');
+
+let domElementDice = document.getQuerySelector('dice');
+
+// set initial conditions
+domElementScore0.textValue = 0;
+domElementScore1.textValue = 0;
+
+domElementDice.classList.add('hidden') = 0;
