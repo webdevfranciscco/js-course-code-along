beeead5c12e5fbe058c3422e29e7060acdcfbaf1
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-19 14:58:42 -0600

    examples of declaration, expression and arrow functions called before definition

--- 08-how-js-works-behind-the-scenes/hoisting.js
+++ 08-how-js-works-behind-the-scenes/hoisting.js
@@ -1,6 +1,8 @@
 'use strict';
+
 // Variables
 
+/*
 console.log(me); // undefined
 console.log(job); // Uncaught ReferenceError: Cannot access 'job' before initialization
 console.log(year); // Uncaught ReferenceError: Cannot access 'job' before initialization
@@ -8,3 +10,19 @@ console.log(year); // Uncaught ReferenceError: Cannot access 'job' before initia
 var me = 'Jonas';
 let job = 'teacher';
 const year = 1991;
+*/
+
+// Functions
+
+console.log(addDeclaration(2, 3)); // 5
+console.log(addExpression(2, 3)); // Uncaught ReferenceError: Cannot access 'addExpression' before initialization
+console.log(addArrow(2, 3)); // Uncaught ReferenceError: Cannot access 'addExpression' before initialization
+function addDeclaration(a, b) {
+  return a + b;
+}
+
+const addExpression = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
