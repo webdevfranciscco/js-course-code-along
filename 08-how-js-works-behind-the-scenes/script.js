3c40eb9ebfc2f00cf25430d54bb9fb7a4904b431
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-18 21:17:53 -0600

    add starter files for section "how js works behind the scenes"

---
+++ 08-how-js-works-behind-the-scenes/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge();
