fc09c32ff1023018b7720f0a854eef794e2944ee
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-06-25 18:29:25 -0600

    add a variable declared with "var" and explain in the comments how that affects

--- 08-how-js-works-behind-the-scenes/this-keyword-pitfalls.js
+++ 08-how-js-works-behind-the-scenes/this-keyword-pitfalls.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// adding this "var" declaration makes the property "firstName" available to the "jonas.greet()"" call
+// this happens because "var" adds "firstName" as a property to the Window Object, which is what "this" is set up to in "jonas.greet()"
+var firstName = 'Matilda'; // <-- we only added this line
+// and the result is significantly different
 const jonas = {
   firstName: 'Jonas',
   year: 1991,
@@ -18,4 +22,4 @@ const jonas = {
   },
 };
 
-jonas.greet(); // this call logs 'Hey undefined' to the console
+jonas.greet(); // this call logs 'Hey Matilda' instead of 'Hey undefined' to the console, by pulling "Matilda" from the firstName property of the Window Object
