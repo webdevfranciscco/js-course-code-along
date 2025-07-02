13320dce4b9e1244ec6e6f15cf8ecd15d54e65ba
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-07-02 17:36:01 -0600

    an example of this not being as expected

--- 08-how-js-works-behind-the-scenes/this-keyword-pitfalls.js
+++ 08-how-js-works-behind-the-scenes/this-keyword-pitfalls.js
@@ -1,22 +1,24 @@
 'use strict';
 
-const jonas = {
-  firstName: 'Jonas',
-  year: 1991,
-  calcAge: function () {
-    // console.log(this);
-    console.log(2037 - this.year);
-  },
+// isAdult0 will fail in this code:
 
-  greet: function () {
-    console.log('The value of "this" within greet():', this);
-    console.log(
-      'The value of "this.firstName" within greet():',
-      this.firstName
-    );
-    console.log(`Hey ${this.firstName}`);
+const jonas0 = {
+  firstName0: 'Jonas',
+  year0: 1991,
+  calcAge0: function () {
+    console.log(2037 - this.year0);
+
+    const isAdult0 = function () {
+      console.log(2037 - this.year0 >= 18);
+    };
+    isAdult0();
   },
 };
 
-jonas.greet(); // Now that the method "greet()" is a regular function expression, "this" is the parent object, firstName is equal to 'Jonas' and the output is 'Hey Jonas'
-jonas.calcAge();
+jonas0.calcAge0();
+// output:
+// >> 46
+// Uncaught TypeError: Cannot read properties of undefined (reading 'year0')
+// at isAdult0 (this-keyword-pitfalls.js:12:31)
+// at Object.calcAge0 (this-keyword-pitfalls.js:14:5)
+// at this-keyword-pitfalls.js:18:8
