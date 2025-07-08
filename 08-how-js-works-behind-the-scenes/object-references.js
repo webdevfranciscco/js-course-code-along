971f73fd8b3291f68c8d84a0dce3a68846e75b73
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-07-07 18:02:01 -0600

    add an example: use of the spread operator to create a true copy of an object

--- 08-how-js-works-behind-the-scenes/object-references.js
+++ 08-how-js-works-behind-the-scenes/object-references.js
@@ -2,8 +2,8 @@
 
 // object reference in practice
 
-const jessica = {
-  objectId: 'jessica',
+const jessica1 = {
+  objectId: 'jessica1',
   firstName: 'Jessica',
   lastName: 'Williams',
   age: 27,
@@ -14,10 +14,46 @@ function marryPerson(originalPerson, newLastName) {
   return originalPerson;
 }
 
-// const marriedJessica = jessica;
-// marriedJessica.lastName = 'Davis';
+const marriedJessica = marryPerson(jessica1, 'Davis');
 
-const marriedJessica = marryPerson(jessica, 'Davis');
-
-console.log('Before:', jessica);
+console.log('Before:', jessica1);
 console.log('After:', marriedJessica);
+
+const jessica2 = {
+  objectId: 'jessica2',
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  siblings: ['Alice', 'Bob'],
+};
+
+const jessica2Copy = { ...jessica2 }; // this copies all the elments of jessica2 into jessica2Copy
+
+console.log(
+  'These objects have the same elements, but are they different objects?'
+);
+console.log('jessica2:', jessica2);
+console.log('jessica2Copy:', jessica2Copy);
+
+// let's make jessica2Copy unique by changing de objectId and the last name
+jessica2Copy.objectId = 'jessica2Copy';
+jessica2Copy.lastName = 'Davis';
+
+// now we can tell they are different objects
+console.log('Clearly, these are different objects:');
+console.log('jessica2:', jessica2);
+console.log('jessica2Copy:', jessica2Copy);
+
+// now let's add a couple siblings to jessica2Copy
+jessica2Copy.siblings.push('Mary');
+jessica2Copy.siblings.push('John');
+
+console.log('When we add 2 more siblings to jessica2Copy, this happens:');
+console.log('jessica2:', jessica2);
+console.log('jessica2Copy:', jessica2Copy);
+console.log(
+  'Both jessica2 and jessica2Copy have the same 4 elements on the "family" array!!'
+);
+console.log(
+  'The spread operator copies all of the properties, but any properties that are also objects are still passed by reference.'
+);
