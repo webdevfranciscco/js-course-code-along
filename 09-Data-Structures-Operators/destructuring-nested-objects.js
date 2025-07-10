254872838afc296a35c97dcdd66e5b4ce1c1d8ec
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-07-10 12:31:04 -0600

    add example: passing an object as an argument and then destructuring it

--- 09-Data-Structures-Operators/destructuring-nested-objects.js
+++ 09-Data-Structures-Operators/destructuring-nested-objects.js
@@ -15,8 +15,27 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({
+    starterIndex = 1, // use = sign to set default value
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at  ${time}`
+    );
+  },
 };
 
+restaurant.orderDelivery({
+  time: '20:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+/*
 const { openingHours } = restaurant;
 
 // nested objects
@@ -33,3 +52,4 @@ const {
   fri: { open: o, close: c },
 } = openingHours;
 console.log('o:', o, 'c:', c);
+*/
