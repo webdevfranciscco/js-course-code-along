3091eba12266ccaa9602eb928d0066a1b2e744c2
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-07-11 14:34:48 -0600

    add theory examples for the rest operator: ...

---
+++ 09-Data-Structures-Operators/rest-operator.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: { open: 12, close: 22 },
+    fri: { open: 11, close: 23 },
+    sat: { open: 0, close: 24 },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1, // use = sign to set default value
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at  ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+};
+
+// REST IN ARRAYS
+
+// Spread when at the right hand side of =
+const arr = [1, 2, ...[3, 4]];
+console.log('arr:', arr);
+
+// Rest when at the left hand side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log('a, b, others:', a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log('pizza, risotto, otherFood:', pizza, risotto, otherFood);
+
+// REST MUST ALWAYS BE THE LAST ELEMENT IN AN ARRAY
+
+// REST IN OBJECTS
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
