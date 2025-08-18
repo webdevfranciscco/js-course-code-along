eb428a16187072679ab9375c2f2ef162830b75fb
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-08-18 12:38:59 -0600

    add example of the sets intersection method

---
+++ 09-Data-Structures-Operators/more-set-operations.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
+
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
+
+const commonFoods = italianFoods.intersection(mexicanFoods);
+
+console.log('Intersection:', commonFoods);
+console.log([...commonFoods]);
