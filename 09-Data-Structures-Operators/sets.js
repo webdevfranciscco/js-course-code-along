00597d12c202d39cf099cf2c67ff2830424d9107
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-08-18 11:01:47 -0600

    add examples of operations with sets

--- 09-Data-Structures-Operators/sets.js
+++ 09-Data-Structures-Operators/sets.js
@@ -4,8 +4,49 @@ const ordersSet = new Set([
   'Pasta',
   'Pizza',
   'Pizza',
-  'Rissotto',
+  'Risotto',
   'Pasta',
   'Pizza',
 ]);
+console.log(ordersSet);
+
+console.log(new Set());
+
+console.log(new Set('Jonas'));
+
+console.log(ordersSet.size);
+
+console.log(ordersSet.has('Pizza'));
+
+console.log(ordersSet.has('Bread'));
+
+ordersSet.add('Garlic Bread');
+
+ordersSet.add('Garlic Bread');
+
+ordersSet.delete('Risotto');
+
+console.log(ordersSet);
+
+// this logs "Undefined" it does not work
+// -----------------------------------------------------------------
+console.log(ordersSet[0]);
+
+// getting values out of a set into an array
+// -----------------------------------------------------------------
+console.log('-------------------');
+const array = [];
+let i = 0;
+for (const order of ordersSet) {
+  console.log(`set element[${i}]`, order);
+  array[i] = order;
+  i++;
+}
+
+console.log('array:', array);
+
+console.log('-------------------');
+
+ordersSet.clear(); // this empties the set
+
 console.log(ordersSet);
