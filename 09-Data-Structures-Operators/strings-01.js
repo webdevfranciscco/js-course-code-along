48cbee1cb7bd766f6ea34ec6f88ba4d29b3baada
wbdevfranciscco <webdevfranciscco@gmail.com>
2025-08-20 15:01:15 -0600

    add string operations basics

---
+++ 09-Data-Structures-Operators/strings-01.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.lastIndexOf('Portugal'));
+console.log(airline.lastIndexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
